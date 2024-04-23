edf57d8c60d4062b8db966a6d6e358b7278ab968
Noor ul <hudanoor714,com>
2024-04-23 20:59:47 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+// shabnag
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operator to perfume opration",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// condition statement
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please selet valid operator");
+}
